5f5fdfc9c837c1037dafcb4e7c91f615d95aaeb6
liuheran <liuheran_87@126.com>
2017-03-14 19:22:21 +0800

    Update Contact.php

--- frontend/models/Contact.php
+++ frontend/models/Contact.php
@@ -6,7 +6,7 @@ use Yii;
 
 /**
  * This is the model class for table "Contact".
- * test one two three
+ * test one two three four
  * @property string $id
  * @property string $userId two
  * @property string $contactId
